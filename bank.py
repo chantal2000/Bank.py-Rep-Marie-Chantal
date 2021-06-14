aa00f5067459e058af019bbebcb1a3b605c47da2
chantal2000 <nniyonkuruchantal@gmail.com>
2021-06-14 21:54:17 +0300

    bank Account

--- bank.py
+++ bank.py
@@ -10,6 +10,11 @@ class Account:
     def showbalance(self):
         return  f"Hello {self.name} your balance is {self.balance}"
     def deposit(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Amount must be figures"
+        
         if amount<0:
             return f"amount can't be less than 0"
         else:
@@ -22,6 +27,23 @@ class Account:
             }
             self.statement.append(transaction)
             return self.showbalance()
+    def transfer(self,amount,name):
+        try:
+            10+amount
+        except TypeError:
+            return f"Amount must be figures"
+        fee=amount*0.05
+        total=amount+fee
+        self.balance-=total
+        if amount<0:
+            return f"You can't transfer"
+           
+        elif total>self.balance :
+            return f"your balance is {self.balance} and You need atleast {total}"
+        else:
+            self.balance-=total
+            return f"Your balance is {self.balance}"
+
     def show_statement(self):
         for transaction in self.statement:
                     amount=transaction["amount"]
@@ -32,6 +54,10 @@ class Account:
         return 
         
     def withdraw(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Amount must be figures"
         if amount>self.balance:
             return f"Your balance is {self.balance} and You can not withdraw {amount}"
         else:
@@ -54,6 +80,10 @@ class Account:
         return 
         
     def borrow(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Amount must be figures"
         if amount<0:
             return "You can't borrow"
         elif self.loan>0:
@@ -75,6 +105,10 @@ class Account:
 
 
     def repayloan(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Amount must be figures"
         if amount<0:
             return "You have to pay"
         elif amount<self.loan:
@@ -92,6 +126,47 @@ class Account:
             }
             self.statement.append(transaction)
             return f"{self.showbalance()} and your loan is completely cleared"
+class MobileMoneyAccount(Account):
+    def __init__(self,name,phonenumber,loan,service_provider):
+        Account.__init__(self,name,phonenumber,loan)
+        self.service_provider=service_provider
+
+    def buy_airtime(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Amount must be figures"
+        if amount<0:
+            return f"Your balance is negative and You can't buy airtime"
+        elif amount<self.balance:
+            return f"Dear Customer Your balance is not sufficient;deposit money and try again"
+        else:
+            self.balance=self.balance-amount
+            return f"Dear Customer; Your can Buy airtime and your balance is {self.balance} "
+    def withdraw(self,amount):
+        try:
+            30000+amount
+        except TypeError:
+            return f"Amount must be figures"
+        if amount>self.balance:
+            return f"Your balance is {self.balance} and You can not withdraw {amount}"
+        else:
+            self.balance-=amount
+            now=datetime.now()
+            transaction={
+                "amount":20,
+                "time":now,
+                "narration":"You withdrew"
+            }
+            self.statement.append(transaction)
+            return self.showbalance()
+        
+
+
+
+
+
+        
 
 
 
